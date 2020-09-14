93ededd914128c2df037e5972a697d41e9dc5e7a
Aaron <agkendrick1@gmail.com>
2020-09-14 17:27:29 -0500

    Updates

---
+++ precache-manifest.c249d898b4d2201a1b95ab03f70baddd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49ac68eaedb3b342fc8a4da192ca0d21",
+    "url": "/jotform/index.html"
+  },
+  {
+    "revision": "f8e62f25a1e6edcff00a",
+    "url": "/jotform/static/css/main.b08df608.chunk.css"
+  },
+  {
+    "revision": "48ceb8159c6cfdb33c11",
+    "url": "/jotform/static/js/2.c07e963c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jotform/static/js/2.c07e963c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8e62f25a1e6edcff00a",
+    "url": "/jotform/static/js/main.175f1fd3.chunk.js"
+  },
+  {
+    "revision": "0208b31ad9dcaa8aa5a4",
+    "url": "/jotform/static/js/runtime-main.e3b00b43.js"
+  }
+]);
